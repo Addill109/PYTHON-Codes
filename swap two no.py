16d0be719bbb60bdd3c1e4724e1d227ea6a40edc
Addill109 <adiliqbal121410@gmail.com>
2026-04-22 17:32:27 +0530

    Create swap two no.py

---
+++ swap two no.py
@@ -0,0 +1,8 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+temp = x
+x = y
+y = temp
+print("After swapping:")
+print("First number:", x)
+print("Second number:", y)
